15813ce459fead57a6dd4822b65a23b41171bd70
202244041 <mhso0205@naver.com>
2025-03-17 13:09:04 +0900

    첫 번째 커밋

---
+++ numbercatch.py
@@ -0,0 +1,24 @@
+import random
+
+count = 0       # 현재 입력된 횟수
+chance = 10     # 총 입력 기회
+
+number = random.randint(1, 100)
+
+print("1부터 99까지의 숫자를 10번 안에 맞춰보세요.")
+
+while count < chance:
+    count += 1
+    user_input = int(input("몇 일까요?> "))
+
+    if user_input == number:
+        break
+    elif user_input < number:
+        print("{} 보다 큰 숫자 입니다.".format(user_input))
+    elif user_input > number:
+        print("{} 보다 낮은 숫자 입니다.".format(user_input))
+
+if user_input == number:
+    print("성공!! {} 이 맞습니다.".format(number))
+else:
+    print("실패, 정답은 {} 입니다.".format(number))
