db2170573eb269b1d13df390cb3d133de8cc9d9f
202244041 <mhso0205@naver.com>
2025-04-07 11:27:14 +0900

    자료구조 4주차

---
+++ week5.py
@@ -0,0 +1,33 @@
+class Stack:
+    def __init__(self):
+        self.items = list()
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        return self.items[-1]
+
+
+s1 = Stack()
+s2 = Stack()
+print(s1.is_empty())
+s1.push("자료구조")
+print(s1.is_empty())
+print(s2.is_empty())
+s1.push("데이터베이스")
+print(s1.size())
+print(s1.peek())
+print(s1.size())
+print(s1.pop())
+print(s1.size())
+print(s1.peek())
