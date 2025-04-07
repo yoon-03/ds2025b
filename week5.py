3181f24daadbfb61abf318c83cf02620937acb3d
202244041_B <mhso0205@naver.com>
2025-04-07 11:45:37 +0900

    자료구조 5주차

--- week5.py
+++ week5.py
@@ -31,3 +31,20 @@ print(s1.size())
 print(s1.pop())
 print(s1.size())
 print(s1.peek())
+
+
+
+ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ리스트버전ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+s1 = list()
+print(len(s1))
+s1.append("Data structure")  # push
+s1.append("DataBase")  # push
+print(len(s1))  # size
+print(s1[-1])  # peek
+print(s1)
+print(s1.pop())
+print(s1)
+print(s1.pop())
+print(s1)
+# print(s1.pop())
+# print(s1)
